b3fcce0e3aeea6e1b4f0c726b967504f01a3030e
DanielZhong24 <danielzhong0618@gmail.com>
2025-11-16 17:11:55 -0500

    11-16

---
+++ 0033_search_in_rotated_sorted_array.py
@@ -0,0 +1,12 @@
+#2025-11-16 time: 2:40
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if target in nums:
+            return nums.index(target)
+        else:
+            return -1
